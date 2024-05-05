832cab9330a73e64dc8de326660496599e3713d6
ame <ame@abyss.moe>
2024-05-05 17:01:32 +0900

    🦊 Add nuxt-umami

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+    umami: {
+        host: 'https://akari.abyss.moe/',
+        id: 'e8c38a63-0d7a-4a55-9405-06d4eac85810',
+        version: 2,
+        ignoreLocalhost: true,
+        debug: true,
+    },
+});
