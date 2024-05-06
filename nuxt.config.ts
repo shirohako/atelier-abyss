0fcbb2024ea5236b06100fe621e0fcf56d1ec3e6
ame <ame@abyss.moe>
2024-05-06 13:32:29 +0900

    🦊 更新标题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,12 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
   ],
-  extends: ['nuxt-umami']
+  extends: ['nuxt-umami'],
+  app: {
+    head: {
+      title: 'Atelier Abyss',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  }
 })
