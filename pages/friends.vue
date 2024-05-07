fe6ce053ff5bf65bae191957c7e75ba21e5ca472
ame <ame@abyss.moe>
2024-05-07 23:39:22 +0900

    🦊 暂时修复友链的缓存时间过长

--- pages/friends.vue
+++ pages/friends.vue
@@ -13,12 +13,19 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+let friends = ref([]);
+
 useSeoMeta({
-  description: "Adorable pals who exchanged links with me | 御伽噺を耳にして、思い巡らす其れ以上に。生きる幻想が其処に居る。",
+  description:
+    "Adorable pals who exchanged links with me | 御伽噺を耳にして、思い巡らす其れ以上に。生きる幻想が其処に居る。",
 });
 
 const friendDataJson = await $fetch(
-  "https://cdn.jsdelivr.net/gh/shirohako/helm@latest/Friends.json"
+  "https://raw.githubusercontent.com/shirohako/helm/master/Friends.json"
 );
-const friends = friendDataJson["groups"][0]["assets"];
+
+friends = JSON.parse(friendDataJson);
+friends = friends["groups"][0]["assets"];
 </script>
