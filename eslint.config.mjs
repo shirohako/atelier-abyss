c522eed5da28049713e76dc9d5ff0afe7e84bbaa
ame <ame@abyss.moe>
2024-05-11 19:51:41 +0900

    🐺 添加 eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+  rules: {
+    "no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon',
-  ],
-  extends: ['nuxt-umami'],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/eslint"],
+  extends: ["nuxt-umami"],
   app: {
     head: {
-      title: 'Atelier Abyss',
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      htmlAttrs: { lang: 'en' }
-    }
-  }
-})
+      title: "Atelier Abyss",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      htmlAttrs: { lang: "en" },
+    },
+  },
+});
