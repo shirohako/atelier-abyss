9ff0284fcd2092204561649ff6c381d5f40732d6
ame <ame@abyss.moe>
2024-05-11 23:54:39 +0900

    🐺 修改 eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt({
   rules: {
     "no-unused-vars": "warn",
+    "vue/html-self-closing": "off",
+    "vue/multi-word-component-names": "off",
     "@typescript-eslint/no-unused-vars": "off",
   },
 });
