40131b42cfeb16ca274e08e57df00d18056d8715
ame <ame@abyss.moe>
2024-08-11 03:46:09 +0900

    🦊 Add link target

--- pages/friends.vue
+++ pages/friends.vue
@@ -3,9 +3,12 @@
     <h2 class="text-2xl font-bold">友链存放地</h2>
     <div class="bg-blue-100 p-5 my-3 rounded-md text-gray-600">
       交换友链/修改信息：请在
-      <a href="https://github.com/shirohako/helm" class="font-bold"
-        >Github 仓库</a
-      >
+      <a
+        href="https://github.com/shirohako/helm"
+        target="_blank"
+        class="font-bold"
+        >Github 仓库
+      </a>
       创建新的 issue 留言即可
     </div>
     <div class="grid lg:grid-cols-2 grid-cols-1 gap-x-3 gap-y-6 mt-5">
