f9aa967920fbc3a0881c232cd0331513443b9072
ame <ame@abyss.moe>
2024-08-11 00:14:25 +0900

    🦊 更新依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/eslint"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/eslint"],
   extends: ["nuxt-umami"],
   app: {
     head: {
